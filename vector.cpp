7158fab29a5bf540f15e5c88fdd76ecb9571635a
özgür <44574065+ozgurcancal@users.noreply.github.com>
2020-04-25 22:26:00 +0300

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,864 @@
+﻿// vector.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Vector.h"
+#include <vector>
+#include <time.h>
+#include <conio.h>
+
+Vector::Vector() noexcept :arr{ new int[0] }, mysize{ 0 }, mycap{ 0 }{
+
+
+}
+
+Vector::~Vector() {
+
+    delete[] arr;   
+    arr = nullptr;    
+   
+}
+
+Vector::Vector(size_t size, int val) {  
+
+    mysize, mycap = size;
+    arr = new int[mycap] {val};
+}
+
+void Vector::copy_Vector(const Vector& Obj) {
+
+    if (Obj.mycap > mycap) {
+        reserve(Obj.mycap);  //kopyalanacak arraya gore yer(kapasite) acar
+        mysize = Obj.mysize;
+        std::copy(Obj.arr, Obj.arr + Obj.mysize, arr);
+    }
+
+    else if (mycap == Obj.mycap) {   // shrink_to_fit 
+
+        arr = new int[Obj.mysize];
+        mycap = Obj.mysize;
+
+        std::copy(Obj.arr, Obj.arr + Obj.mysize, arr);
+        // 
+         //
+    }
+
+}
+
+Vector::Vector(const Vector& Obj) {
+
+    copy_Vector(Obj);
+
+}
+
+Vector::Vector(Vector&& Obj)noexcept { 
+
+    copy_Vector(Obj);
+    delete[] Obj.arr;
+    Obj.arr = nullptr;
+  
+
+}
+
+Vector& Vector::operator=(const Vector& Obj) {
+
+    copy_Vector(Obj);
+    return *this;
+}
+
+Vector& Vector::operator=(Vector&& Obj)noexcept {
+
+    copy_Vector(Obj);
+    delete[] Obj.arr;
+    Obj.arr = nullptr;
+    return *this;
+}
+
+Vector::Vector(std::initializer_list<int> ilist) {  
+
+    mysize = ilist.size();
+    mycap = mysize * 2;
+    arr = new int[mycap];
+    std::initializer_list<int>::iterator it;  
+    int i = 0;
+    for (it = ilist.begin(); it != ilist.end(); ++it) {
+        arr[i] = *it;
+        i++;
+    }
+
+}
+
+template <typename T>
+void Vector::initialize_in_range(T x, T y) {
+
+    if (!mysize) {
+        mysize = 5;
+        mycap = 10;
+        arr = new int[mycap];
+    }
+
+
+    auto vbegin = const_cast<int*>(x);
+    auto vend = const_cast<int*>(y);
+    int range = *vbegin - *vend;
+
+    for (unsigned i = 0; i < mysize; i++) {
+        arr[i] = (rand() % range) + *vbegin;
+    }
+}
+
+Vector::Vector(const int* pbegin, const int* pend) {
+
+    initialize_in_range<const int*>(pbegin, pend);
+}
+
+Vector::Vector(const_iterator beg, const_iterator end) {
+
+    initialize_in_range<const int*>(beg.p, end.p);
+}
+
+void Vector::reserve(size_t new_cap) {
+
+    if (new_cap > mycap) {
+        int* p = new int[new_cap];
+        for (unsigned i = 0; i < mysize; i++)
+            p[i] = arr[i];
+
+        delete[] arr;
+        arr = p;
+        mycap = new_cap;
+
+    }
+}
+
+void Vector::shrink_to_fit() {
+
+    Vector Obj{ *this };
+    copy_Vector(Obj);
+}
+
+Vector::iterator Vector::begin() {
+
+    Vector::iterator it;
+    it.p = arr;
+    if (!mysize)
+        throw ("array bos"); 
+    return it;
+}
+
+Vector::iterator Vector::end() { 
+
+    Vector::iterator it;
+    it.p = arr + mysize;
+    if (!mysize)
+        throw ("array bos"); 
+    return it;
+}
+
+Vector::const_iterator Vector::begin()const {
+
+    Vector::const_iterator it;
+    it.p = arr;
+
+    return it;
+}
+
+Vector::const_iterator Vector::end()const {
+
+    Vector::const_iterator it;
+    it.p = arr + mysize;
+
+    return it;
+}
+
+void Vector::copy_ilist(std::initializer_list<int> ilist) {
+
+    if (mycap < ilist.size()) {
+        if (!mysize)
+            delete[] arr;
+        arr = new int[ilist.size() * 2];
+        mysize = ilist.size();
+        mycap = 2 * mysize;
+    }
+    std::copy(ilist.begin(), ilist.end(), arr);
+}
+
+Vector& Vector::operator=(std::initializer_list<int> ilist) {
+
+    copy_ilist(ilist);
+
+    return *this;
+}
+
+void Vector::resize(size_t new_size, int val) { 
+
+    if (new_size > mycap)
+        reserve(2 * new_size);
+
+    else
+    {
+        if (mysize > new_size)
+            mysize -= mysize - new_size;
+
+        else if (mysize < new_size) {
+            auto size_difference = new_size - mysize;
+            unsigned i = 0;
+            while (i < size_difference) {
+                arr[mysize + i] = val;
+                i++;
+            }
+            mysize = new_size;
+        }
+    }
+
+}
+
+void Vector::assign(size_t n, int m) {
+
+    mycap = n * 2;
+    mysize = n;
+    arr = new int[n * 2];
+
+    unsigned i = 0;
+    while (i < n) {
+
+        arr[i] = m;
+        i++;
+    }
+}
+
+void Vector::assign(std::initializer_list<int> ilist) {
+
+    copy_ilist(ilist);
+}
+
+void Vector::assign(const int* pbeg, const int* pend) {
+
+    initialize_in_range(pbeg, pend);
+}
+
+Vector::iterator Vector::insert(iterator where, int val) {	
+
+    unsigned size = mysize;
+    unsigned pos = where.p - arr;
+    while (pos < size) {     //kaydır
+        arr[size] = arr[size - 1];
+        size--;
+    }
+    arr[pos] = val;   //ekle
+    mysize++;
+    return where;
+}
+
+Vector::iterator Vector::insert(iterator where, iterator source_beg, iterator source_end) {
+    int* it = source_beg.p;
+
+    unsigned size = mysize;
+    unsigned pos = where.p - arr;
+    unsigned x = source_end.p - source_beg.p;
+
+    mysize += x;
+    while (pos < size) {  //pozisyondan itibaren arrayi eklenen eleman sayısı kadar sağa çek
+        arr[size + x - 1] = arr[size - 1];
+        size--;
+    }
+    unsigned i = pos;
+    while (it < source_end.p) {  //elemanları ekle
+        arr[i] = *it;
+        i++; it++;
+    }
+    return source_beg;
+}
+
+Vector::iterator Vector::erase(iterator where) {
+
+    unsigned pos = where.p - arr;
+    unsigned i = pos;
+    while (i < mysize) {
+        arr[i] = arr[i + 1];
+        i++;
+    }
+    mysize--;
+    return where;
+}
+
+Vector::iterator Vector::erase(iterator where, iterator source_beg, iterator source_end) {
+
+    unsigned pos = where.p - arr;
+    unsigned x = source_end.p - source_beg.p;
+
+    unsigned i = pos;
+    while (i < mysize) {
+        arr[pos + i] = arr[pos + i + x];
+        i++;
+    }
+    mysize -= x;
+
+    return where;
+}
+
+void Vector::push_back(int val) {
+
+    arr[mysize] = val;
+    mysize++;
+}
+
+void Vector::pop_back() {
+
+    mysize--;
+}
+
+void Vector::swap(Vector& Obj) {  
+    unsigned i = 0, temp;
+    auto size = mysize > Obj.mysize ? mysize : Obj.mysize;
+
+    while (i < size)
+    {
+        temp = arr[i];
+        arr[i] = Obj.arr[i];
+        Obj.arr[i] = temp;
+        i++;
+    }
+
+    temp = mysize;
+    mysize = Obj.mysize;
+    Obj.mysize = temp;
+}
+
+void Vector::clear() {
+
+    delete[] arr;
+    arr = new int[0];
+    mycap, mysize = 0;
+
+}
+
+int& Vector::front() {
+
+    if (int* p = arr; mysize)
+        return *p;
+    else
+        throw("vector bos ");
+}
+
+const int& Vector::front()const {
+
+    if (int* p = arr; mysize)
+        return *p;
+    else
+        throw("vector bos ");
+}
+
+int& Vector::back() {
+
+    if (int* p = arr + mysize - 1; mysize)
+        return *p;
+    else
+        throw("vector bos ");
+}
+
+const int& Vector::back()const {
+
+    if (int* p = arr + mysize - 1; mysize)
+        return *p;
+    else
+        throw("vector bos ");
+}
+
+int& Vector::operator[](size_t idx) {
+
+    if (idx < mysize)
+        return arr[idx];
+    else
+        throw("indeks gecerli degil ");
+}
+
+const int& Vector::operator[](size_t idx)const {
+
+    if (idx < mysize)
+        return arr[idx];
+    else
+        throw("indeks gecerli degil ");
+}
+
+int* Vector::data() {
+
+    return arr;
+}
+
+const int* Vector::data()const {
+
+    return arr;
+}
+
+size_t Vector::capacity()const {
+
+    return mycap;
+}
+
+size_t Vector::size()const {
+
+    return mysize;
+}
+
+bool Vector::empty()const {
+
+    if (mysize)
+        return 1;
+    else
+        return 0;
+}
+
+Vector::iterator& Vector::iterator::operator++() {  
+
+    p++;
+
+    return *this;
+}
+
+Vector::iterator Vector::iterator::operator++(int) {
+
+    iterator it{ *this };
+    p++;
+
+    return it;
+}
+
+Vector::iterator& Vector::iterator::operator--() {
+
+    p--;
+
+    return *this;
+}
+
+Vector::iterator Vector::iterator::operator--(int) {
+
+    iterator it{ *this };
+    p--;
+
+    return it;
+}
+
+int& Vector::iterator::operator*() {
+
+    return *p;
+}
+
+int& Vector::iterator::operator[](int n) {
+
+    return *(p + n);
+}
+
+std::ptrdiff_t Vector::iterator::diffiter(const iterator& it) {
+
+    return p - it.p;
+}
+
+Vector::iterator Vector::iterator::operator+(size_t n) { 
+
+    iterator it;
+    it.p += n;
+    return it;
+}
+
+Vector::iterator Vector::iterator::operator-(size_t n) {
+
+    iterator it;
+    it.p -= n;
+    return it;
+}
+
+Vector::iterator& Vector::iterator::operator+=(size_t n) {
+
+    p += n;
+
+    return *this;
+}
+
+Vector::iterator& Vector::iterator::operator-=(size_t n) {
+
+    p -= n;
+    return *this;
+}
+
+bool Vector::iterator::operator<(const iterator& it)const {
+
+    if (*p < *it.p)
+        return 1;
+    else
+        return 0;
+}
+
+bool Vector::iterator::operator<=(const iterator& it)const {
+
+    return !(it < *this);
+}
+//
+bool Vector::iterator::operator>(const iterator& it)const {
+
+    return (it < *this);
+}
+//
+bool Vector::iterator::operator>=(const iterator& it)const {
+
+    return !(*this < it);
+}
+
+bool Vector::iterator::operator==(const iterator& it)const {
+
+    return (*p == *it.p);
+}
+
+bool Vector::iterator::operator!=(const iterator& it)const {
+
+    return !(*this == it);
+}
+
+Vector::iterator& Vector::const_iterator::operator++() {
+
+    iterator it;
+    it.p = const_cast<int*>(p);
+    p++;
+
+    return it;
+
+}
+
+Vector::iterator Vector::const_iterator::operator++(int) {
+
+    iterator it;
+
+    it.p = const_cast<int*>(p);
+
+    p++;
+
+    return it;
+
+}//80
+
+Vector::iterator& Vector::const_iterator::operator--() {
+
+    iterator it;
+
+    it.p = const_cast<int*>(p);
+
+    p--;
+
+    return it;
+
+}
+
+Vector::iterator Vector::const_iterator::operator--(int) {
+
+    iterator it;
+
+    it.p = const_cast<int*>(p);
+
+    p--;
+
+    return it;
+
+}//80
+
+const int& Vector::const_iterator::operator*() {
+
+
+    return *p;
+
+}//80
+
+const int& Vector::const_iterator::operator[](int n) {
+
+
+    return *(p + n);
+
+}//80
+
+std::ptrdiff_t Vector::const_iterator::operator-(iterator it) {
+
+
+    return (p - it.p);
+
+}//80
+
+Vector::const_iterator Vector::const_iterator::operator+(int n)const {
+
+    const_iterator it;
+    it.p = p + n;
+    return (it);
+
+}//80
+
+Vector::const_iterator Vector::const_iterator::operator-(int n)const {
+
+    const_iterator it;
+    it.p = p - n;
+    return (it);
+
+}//80
+
+Vector::const_iterator& Vector::const_iterator::operator+=(int n) {
+
+
+    p += n;
+    return (*this);
+
+}//80
+
+Vector::const_iterator& Vector::const_iterator::operator-=(int n) {
+
+
+    p -= n;
+    return (*this);
+
+}//80
+
+bool Vector::const_iterator::operator<(const const_iterator& it)const {
+
+
+
+    return (*p < *it.p);
+
+}//80
+
+bool Vector::const_iterator::operator<=(const const_iterator& it)const {
+
+
+
+    return !(*it.p < *p);
+
+}//80
+
+bool Vector::const_iterator::operator>(const const_iterator& it)const {
+
+
+
+    return (*it.p < *p);
+
+}//80
+
+bool Vector::const_iterator::operator>=(const const_iterator& it)const {
+
+
+
+    return !(*p < *it.p);
+
+}//80
+
+bool Vector::const_iterator::operator==(const const_iterator& it)const {
+
+
+
+    return (*p == *it.p);
+
+}//80
+
+bool Vector::const_iterator::operator!=(const const_iterator& it)const {
+
+
+
+    return !(*p == *it.p);
+
+}//80
+
+
+int main()try
+{
+
+    //for (unsigned i = 0; i < v.mysize; i++)
+//    std::cout << v.arr[i]<<" ";
+
+/*---------------------Test Codes------------------- */
+
+
+    //Vector::const_iterator it;
+    //Vector::const_iterator it2;
+    //const Vector v{ 1,1,3,4,5 };
+    //it.p = v.arr;
+    //it2.p = v.arr + 1;
+    //std::cout << (it >= it2) << '\n';
+    //std::cout << (it > it2) << '\n';
+    //std::cout << (it < it2) << '\n';
+
+
+    //Vector::iterator it;
+    //Vector::iterator it2;
+    //const Vector v{ 1,2,3,3,5 };
+    //it.p = v.arr + 2;
+    //it2.p = v.arr + 3;
+    //std::cout << (it2 <= it);
+
+
+    //Vector::iterator it;
+    //Vector::iterator it2;
+    //const Vector v{ 1,2,3,4,5 };
+    //it.p = v.arr + 2;
+    //it2.p = v.arr + 3;
+    //std::cout << (it2 < it);
+
+    //Vector::iterator it;
+    //const Vector v{ 1,2,3,4,5 };
+    //it.p = v.arr + 2;
+    //it -= 1;
+    //std::cout << *(it).p;
+
+
+    //Vector::iterator it;
+    //Vector::iterator it2;
+    //const Vector v{ 1,2,3,4,5 };
+    //it.p = v.arr;
+    //it2.p = v.arr + 1;
+    //std::cout << it.diffiter(it2);
+
+
+    //Vector::iterator it;
+    //const Vector v{ 1,2,3,4,5 };
+    //it.p = v.arr;
+    //std::cout << *it;
+
+
+    //Vector::iterator it;
+    //const Vector v{ 1,2,3,4,5 };
+    //it.p = v.arr;
+    //std::cout << it[3];
+
+//Vector::iterator it;
+//const Vector v{ 1,2,3,4,5 };
+//it.p = v.arr + 3;
+//
+//++it;
+//
+//if (it.p > v.arr + 4)
+//throw("size ı astin");
+
+
+    //const Vector v{ 1,2,3,4,5 };
+//std::cout << v[0];
+
+//const Vector v{ 1,2,3,4,5 };
+//std::cout << v[0];
+//std::cout << v[0];
+
+
+    //Vector v{ 1,2,3,4,5 };
+    //std::cout << v.front();
+
+
+    //Vector v{ 1,2,3,4,5 };
+    //Vector v1{ 10,11,12,13,14,15,16 };
+    //v.swap(v1);
+
+    //Vector v{ 1,2,3,4,5 };
+    //v.pop_back();
+
+
+    //Vector v{ 1,2,3,4,5 };
+    //v.push_back(6);
+
+
+    //Vector v{ 1,2,3,4,5 };
+    //Vector::iterator where;
+    //Vector::iterator it1;
+    //Vector::iterator it2;
+    //where.p = v.arr;
+    //it1.p = v.arr + 2;
+    //it2.p = v.arr + 4;
+    //v.erase(where, it1, it2);
+
+
+    //Vector::iterator it1;
+    //Vector v{ 1,2,3,4,5 };
+    //it1.p = v.arr;
+    //v.erase(it1);
+
+
+    //Vector v{ 1,2,3,4,5 };
+    //Vector::iterator where;
+    //Vector::iterator it1;
+    //Vector::iterator it2;
+    //where.p = v.arr;
+    //it1.p = v.arr + 2;
+    //it2.p = v.arr + 4;
+    //v.insert(where, it1, it2);
+
+ //Vector v{ 1,2,3,4,5 }; 
+//Vector::iterator it;
+//it.p1 = v.arr+2;
+//it.p2 = v.arr+2;
+//v.insert(it, 9);
+
+    //Vector v{ 1,2,3,4,5 };  sonuc var takılıyor ama.??
+//Vector::iterator it;
+//it.p = v.arr+2;
+//v.insert(it, 9);
+
+    //int x = 1, y = 10;
+    //Vector v;
+    //v.assign(&x, &y);
+
+
+    //Vector v;
+    //v.assign({ 1,2,3,4,5 });
+
+
+    //Vector v;
+    //v.assign(5, 1);
+
+
+    //Vector v;
+    //v = { 1,2,3 };
+    //v.resize(5, 2);
+
+
+    //Vector v;
+    //v = { 1,2,3 };
+
+    //Vector v{ 3,4,5 };
+    //std::cout << v.mycap << " ";
+    //v.shrink_to_fit();
+    //std::cout << v.mycap << "\n";
+
+    //Vector v1{ 3,4,5 };
+    //Vector v;
+    //v = std::move(v1);
+
+    //Vector v1{ 3,4,5 };
+    //Vector v;
+    //v = v1;
+
+    //Vector v1{ 3,4,5 };
+    //Vector v{ (v1) };
+
+ /*   Vector v1{ 3,4,5 };
+    Vector v{ std::move(v1) };*/
+
+    //int x = 1, y = 10;  
+//Vector::const_iterator beg,end ;
+//beg.p = &x;
+//end.p = &y;
+//Vector v(beg, end);
+
+
+    //int x = 1, y = 10;
+    //Vector v(&x, &y);
+
+
+
+ //   Vector v{ 1,2,3 };
+
+
+    //std::vector<int> vec{ 10,20,30 };
+    //Vector::iterator it;
+    //it.p = &vec[0];
+    //
+    //std::cout << *(it++.p);
+    //std::cout << *(it.p);
+
+
+}
+catch (const char* error) {
+    std::cout << " hata yakalandi: " << error;
+    exit(EXIT_FAILURE);
+}
+
+
---
+++ vector.h
@@ -0,0 +1,172 @@
+﻿#pragma once
+
+#include <iostream>
+
+
+class Vector {
+private:
+	void copy_ilist(std::initializer_list<int> ilist);   //std::initializer listi kopyalar
+public:
+
+	int* arr = nullptr;
+	size_t mysize = 0;
+	size_t mycap = 0;
+
+
+	class iterator;//1. İçsel(nested) iterator türü.Kaptaki öğelerin konumlarını tutan iterator nesnelerinin türü.Okuma ve yazma erişimi sağlar.
+
+	class const_iterator;//2. İçsel(nested) const_iterator türü.Kaptaki öğelerin konumlarını tutan const_iterator nesnelerinin türü.Yalnızca okuma amaçlı erişim sağlar.
+
+
+
+	Vector()noexcept;//3. Varsayılan kurucu işlev(default constructor).Boş bir Vector nesnesi oluşturur.
+//
+	~Vector();//4. Sonlandırıcı işlev(destructor).
+//
+	void copy_Vector(const Vector& Obj);
+
+	Vector(const Vector& Obj);///5. Kopyalayan kurucu işlev. (copy constructor)
+//
+	Vector(Vector&& Obj)noexcept;//6. Taşıyan kurucu işlev. (move constructor)
+//
+	Vector& operator=(const Vector& Obj);//7. Kopyalayan atama işlevi. (copy assignment)
+//
+	Vector& operator=(Vector&& Obj)noexcept;//8. Taşıyan atama işlevi. (move assignment)
+//
+	Vector(size_t size, int val = 0);//9. Kurucu işlev.Vector'ü değeri val olan size tane öğe ile başlatır (fill constructor).
+//
+	Vector(std::initializer_list<int> ilist);//10. std::initializer_list parametreli kurucu işlev.Vector nesnesini listedeki değerleri tutacak şekilde başlatır.
+//
+	template <typename T>  // Vektor nesnesine  belirtilen aralıkta ilk deger vererir .
+	void initialize_in_range(T x, T y);
+
+	Vector(const int* pbegin, const int* pend);//11. Aralık(range) parametreli kurucu işlev.Vector nesnesini[pbegin, pend) aralığındaki değerlerle başlatır.Aralık olarak doğrudan adresler(pointer) kullanılmaktadır.
+//
+	Vector(const_iterator beg, const_iterator end);//12. Aralık(range) parametreli kurucu işlev.Vector nesnesini[beg, end) aralığındaki değerlerle başlatır.Aralık olarak const_iterator değerleri kullanılmaktadır.
+//
+	void reserve(size_t new_cap);//13. reserve işlevi.Eğer new_cap değeri var olan kapasiteden büyükse kapasiteyi arttırır.Eğer new_cap değeri var olan kapasiteden küçükse kapasiteyi küçültür.
+//
+	void shrink_to_fit();	//14. Kapasite değerini Vector'de tutulan öğe sayısına (size) büzer (kapasiteyi küçültür).
+
+	iterator begin();	//15. Vector'de tutulan ilk öğenin konumunu döndürür. İşlevin geri dönüş değeri içsel bir tür olan iterator türüdür. Boş bir Vector'de bu konumun içerik(dereferencing) operatörünün terimi yapılması tanımsız davranıştır.
+
+	iterator end();	//16. Vector'de tutulan son öğeden sonraki (olmayan öğenin) konumunu döndürür. İşlevin geri dönüş değeri içsel bir tür olan iterator türüdür. Bu işlevden alınan konum karşılaştırma operatörleri ile diğer konumlarla karşılaştırılabilir. Bu konumun içerik (dereferencing) operatörünün terimi yapılması tanımsız davranıştır.*/
+
+	const_iterator begin()const;	//17. Vector'de tutulan ilk öğenin konumunu (salt okuma erişimli) döndürür. İşlevin geri dönüş değeri içsel bir tür olan const_iterator türüdür. Boş bir Vector'de bu konumun içerik(dereferencing) operatörünün terimi yapılması tanımsız davranıştır.
+
+	const_iterator end()const;	//18. Vector'de tutulan son öğenin konumunu (salt okuma erişimli) döndürür. İşlevin geri dönüş değeri içsel bir tür olan const_iterator türüdür. Boş bir Vector'de bu konumun içerik(dereferencing) operatörünün terimi yapılması tanımsız davranıştır.
+
+	Vector& operator=(std::initializer_list<int> ilist);//22. initializer_list parametreli atama işlevi.Bu işlevin çağrılması ile Vector artık parametre olan listedeki değerleri tutar.
+
+	void resize(size_t new_size, int val = 0);//	23. resize işlevi.Vector'de tutulan öğe sayısını değiştirir. Bu işlev Vector'deki öğe sayısını hem arttırmak hem de azaltmak için kullanılabilir.İşlevin varsayılan argüman alan ikinci parametresi Vector'deki öğe sayısının arttırılması durumunda yeni eklenecek öğelerin alacakları değerdir. Vector'deki öğe sayısından daha küçük bir değerle çağrılırsa sondan silme işlemi yapar.
+
+	void assign(size_t n, int m);//	26. assign işlevi.Bu işlevin çağrılmasıyla Vector nesnesi n tane val değeri tutar hale gelir. (fill assign)
+
+	void assign(std::initializer_list<int> ilist);//	27. initializer_list parametreli assign işlevi.artık Vector parametresine gelen listedeki değerleri tutar.
+
+	void assign(const int* pbeg, const int* pend);	//28. aralık(range)_ parametreli assign işlevi. artık Vector parametresine gelen aralıktaki değerleri tutar.
+
+	iterator insert(iterator where, int val);	//30. where konumuna val değerini ekler.İşlevin geri dönüş değeri eklenmiş öğenin konumu .
+
+	iterator insert(iterator where, iterator source_beg, iterator source_end);//31. where konumuna[beg end) aralığındaki değerleri ekler.İşlevin geri dönüş değeri ilk eklenmiş öğenin konumu .
+
+	iterator erase(iterator where);	//34. where konumundaki öğeyi silmeli.İşlevin geri dönüş değeri silinmiş öğeden sonraki öğenin konumu olmalı
+
+	iterator erase(iterator where, iterator source_beg, iterator source_end);	//35.[beg end) aralığındaki değerleri siler.İşlevin geri dönüş değeri silinen öğelerden sonraki öğenin konumu .
+
+	void push_back(int val); 	//36. push_back işlevi.Parametresine gelen değeri Vector'e son öğe olarak ekler.
+
+	void pop_back();	//37. pop_back işlevi.Vector'deki son öğeyi siler.
+
+	void swap(Vector& Obj);	//38. swap işlevi iki Vector'ü takas eder. 
+
+	void clear();	//39. clear işlevi iki Vector'deki tüm öğeleri silmeli yani Vector'ü boşaltmalır.
+
+	int& front();	//42. front işlevi Vector'de tutulan ilk öğeyi döndürür. Vector'ün boş olması durumunda bu işlevin çağrılması tanımsız davranıştır.
+
+	const int& front()const;	//43. front işlevi Vector'de tutulan ilk öğeye const referans döndürür (const overloading). Vector'ün boş olması durumunda bu işlevin çağrılması tanımsız davranıştır.
+
+	int& back();	//44. back işlevi Vector'de tutulan son öğeyi döndürür. Vector'ün boş olması durumunda bu işlevin çağrılması tanımsız davranıştır.
+
+	const int& back()const;	//45. back işlevi Vector'de tutulan ilk öğeye const referans döndürür (const overloading). Vector'ün boş olması durumunda bu işlevin çağrılması tanımsız davranıştır.
+
+	int& operator[](size_t x);	//46. operator[] işlevi.Vector'de tutulan idx indisli öğeyi döndürür. idx değerinin geçerli olmaması tanımsız davranıştır.
+
+	const int& operator[](size_t x)const;	//47. operator[] işlevi.Vector'de tutulan idx indisli öğeyi (salt okuma erişimli) döndürmeli. (const overloading) idx değerinin geçerli olmaması tanımsız davranıştır.
+
+	int* data();	//50. data işlevi Vector'de tutulan ilk öğenin adresini döndürür. Bu adres C işlevlerine bir dizi adresi olarak gönderilebilir.
+
+	const int* data()const;	//51. data işlevi Vector'de tutulan ilk öğenin adresini (salt okuma erişimli) döndürmeli. Bu adres C api'lerine bir dizi adresi olarak gönderilebilir.
+
+	size_t capacity()const;	//52. capacity işlevi Vector'ün kapasite değerini döndürür. Kapasite değeri Vector nesnesinin edindiği ve tutmakta olduğu dinamik bellek bloğunun öğe sayısı cinsinden büyüklüğüdür.
+
+	size_t size()const;	//53. size işlevi Vector'ün size değerini döndürür. size değeri Vector nesnesinin tutmakta olduğu öğe sayısıdır.
+
+	bool empty()const;	//54. empty işlevi Vector'ün boş olup olmadığını sınar.
+
+};
+
+class Vector::iterator {
+public:
+	int* p = nullptr;
+
+	iterator& operator++();	//60. Ön ek ++ operatörü.iterator nesnesini 1 arttırarak bir sonraki öğenin konumunu tutmasını sağlar.
+
+	iterator operator++(int);	//61. Son ek ++ operatörü.iterator nesnesini 1 arttırarak bir sonraki öğenin konumunu tutmasını sağlar.
+
+	iterator& operator--();		//62. Ön ek -- operatörü.iterator nesnesini 1 eksilterek bir önceki öğenin konumunu tutmasını sağlar.
+
+	iterator operator--(int);	//63. Son ek -- operatörü.iterator nesnesini 1 eksilterek bir önceki öğenin konumunu tutmasını sağlar.
+
+	int& operator*();	//64. İçerik operatörü.iterator nesnesinin tuttuğu konumdaki öğeye eriştirir.
+
+	int& operator[](int n);	//65. İndeks operatörü.iterator nesnesinin tuttuğu konumdaki öğeden n sonraki ya da önceki öğeye eriştirir. 
+
+	std::ptrdiff_t diffiter(const iterator& it); 	//66. İki iterator arasındaki farkı döndürür.
+
+	iterator operator+(size_t n);	//67. iterator konumundan n sonraki konumu döndürür
+
+	iterator operator-(size_t n);	//68. iterator konumundan n önceki konumu döndürür.
+
+	iterator& operator+=(size_t n);	//69. iterator nesnesini n sonraki nesnenin konumunu tutacak şekilde arttırır.
+
+	iterator& operator-=(size_t n);	//70. iterator nesnesini n önceki nesnenin konumunu tutacak şekilde eksiltir.
+
+	bool operator<(const iterator&)const;	//71. iterator nesnelerinin karşılaştırılmalarını sağlayan karşılaştıma operatör işlevleri.
+
+	bool operator<=(const iterator&)const; //71
+
+	bool operator>(const iterator&)const; //71
+
+	bool operator>=(const iterator&)const; //71
+
+	bool operator==(const iterator&)const; //71
+
+	bool operator!=(const iterator&)const; //71
+
+
+};
+
+class Vector::const_iterator {
+public:
+	const int* p;
+
+
+	iterator& operator++();  //80     
+	iterator operator++(int); //81
+	iterator& operator--();  //82
+	iterator operator--(int); //83
+	const int& operator*(); //84
+	const int& operator[](int n);  //85
+	ptrdiff_t operator-(iterator); //86
+	const_iterator operator+(int n)const; //87
+	const_iterator operator-(int n)const; //88
+	const_iterator& operator+=(int n);  //89
+	const_iterator& operator-=(int n);//90
+	bool operator<(const const_iterator&)const; //91
+	bool operator<=(const const_iterator&)const; //91
+	bool operator>(const const_iterator&)const; //91
+	bool operator>=(const const_iterator&)const; //91
+	bool operator==(const const_iterator&)const; //91
+	bool operator!=(const const_iterator&)const; //91
+};
